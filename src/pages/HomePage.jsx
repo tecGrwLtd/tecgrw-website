1c064a13cc6e6af75d3c285b912731e3f9ce21b5
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2025-08-04 19:02:21 +0200

    style: change homepage layout

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -16,7 +16,7 @@ const HomePage = () => {
         description={`We help businesses and farmers level up with the latest tech solutions, while also teaching and guiding students through the same cutting-edge technologies we use in the real world.
 
                      Our aim is to not only shake up how businesses and the agricultural industry work but also to prepare the next generation to confidently navigate the worlds of data and artificial intelligence.`}
-        bgImg="https://images.pexels.com/photos/8386440/pexels-photo-8386440.jpeg?auto=compress&cs=tinysrgb&w=2000&h=1200&fit=crop"
+        bgImg="https://plus.unsplash.com/premium_photo-1734554231510-e9c860f66a60?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         getInTouch={true}
       />
       <Navbar
@@ -24,11 +24,10 @@ const HomePage = () => {
         initialTextColor="text-white"
         changeOnScroll={true}
       />
-        <AboutSection />
+        <PartnersSection />
         <ServicesSection />
         <MetricsSection />
         <AccreditationsSection />
-        <PartnersSection />
     </>
   );
 }
