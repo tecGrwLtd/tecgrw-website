9f56356450af623f61b7c7d002dd9a296b1cdce7
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2025-08-09 07:32:31 +0200

    refactor(contact): split into components and lift state to relevant children

---
+++ src/components/Map.jsx
@@ -0,0 +1,19 @@
+
+const Map = () => {
+  return (
+      <div className="rounded-lg overflow-hidden shadow border border-[#e5e7eb] w-full h-64 md:h-full">
+        <iframe
+          title="Tecgrw Location"
+          src="https://www.google.com/maps?q=100+KG+9+Ave,+Kigali,+Rwanda&output=embed"
+          width="100%"
+          height="100%"
+          style={{ border: 0, minHeight: '220px' }}
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
+    );
+}
+
+export default Map;
