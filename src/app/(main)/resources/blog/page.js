4b041183e7391edb9107ed35a7a93879db085714
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2025-09-02 09:45:43 +0200

    chore(blog): modify names

--- src/app/(main)/resources/blog/page.js
+++ src/app/(main)/resources/blog/page.js
@@ -1,12 +1,11 @@
 import BlogGrid from '@/components/blog/BlogGrid';
 import { getAllBlogs } from '@/lib/blogService';
 
-
 export default async function BlogPage() {
-  const { latestBlog, olderBlogs } = await getAllBlogs();
+  const { recentBlog, olderBlogs } = await getAllBlogs();
 
-  const allBlogs = latestBlog ? [latestBlog, ...olderBlogs] : [];
-
+  const allBlogs = recentBlog ? [recentBlog, ...olderBlogs] : [];
+  // blog grids
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
       <BlogGrid title="Latest Blogs" blogs={allBlogs} />
